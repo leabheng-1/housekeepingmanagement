d722881591f7b75b99481c51747e2929008ea9f6
leab heng <khtop38@gmail.com>
2023-10-09 19:24:15 +0700

    Updated project location

--- lib/dashboard/frontdesk.dart
+++ lib/dashboard/frontdesk.dart
@@ -298,7 +298,7 @@ class _bookingLayoutState extends State<bookingLayout> {
                                     MainAxisAlignment.spaceBetween,
                                 children: [
                                   CustomDropdownFilter(
-                                    parameter: 'YourParameter',
+                                    parameter: 'YourParameter 1',
                                     onChange: (roomStatus, housekeepingStatus,
                                         guestsname) {
                                       roomStatusfilter = roomStatus;
