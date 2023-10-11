2c0b785b51f19ea7523e9798eeeb61fa31412998
leabheng-1 <khtop38@gmail.com>
2023-10-11 19:19:12 +0700

    Updated project location

--- lib/dashboard/report.dart
+++ lib/dashboard/report.dart
@@ -10,7 +10,7 @@ class Report extends StatelessWidget {
       color: Colors.green,
       child: const Center(
         child: Text(
-          'Notification Page sdfsd',
+          'Notification Page',
           style: TextStyle(
               color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0),
         ),
